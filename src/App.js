fccce1d5e9040e2f580ab45d614247abe420d7f8
ahmadhidayat22 <ahmadnurhidayat271@gmail.com>
2024-06-17 00:58:04 +0800

    add comp navbar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+// import './App.css';
+import {
+  NavBar
+} from './components'
+
+function App() {
+  return (
+      <div className='relative z-0'>
+       <NavBar />
+       
+      </div>
+    
+    
+  );
+}
+
+export default App;
